15d59e15366dbc421f8d0c296aa9b72a0d36982d
Rohith V <96191520+RohithV123@users.noreply.github.com>
2024-03-02 20:33:14 +0530

    Add files via upload

---
+++ PhonePe Project/Something.js
@@ -0,0 +1,10 @@
+var First;
+var last;
+class Student{
+    
+        constructor(firstname,lastname){
+            this.First=firstname;
+            this.Last=lastname;
+        }
+}
+var obj1=new Student("Rohith","V");
